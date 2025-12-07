7e3c6873195714dc07abb86d8c8d63df7160f833
VacaMuu7 <valentinvacacipres@gmail.com>
2025-12-07 11:34:15 -0600

    Quit PopUp.

--- src/play.py
+++ src/play.py
@@ -3,6 +3,7 @@ from sprites.player import Player
 from sprites.meteor import Meteor
 from sprites.powerup import PowerUp
 from sprites.fragment import Fragment
+from ui.button import ButtonUI
 from ui.healthbar import HealthBar
 from ui.utils import draw_text, load_fragment_goal
 from ui.counter import TimeCounter
@@ -29,36 +30,50 @@ class Play:
         self.setup_fonts()
         self.setup_ui()
 
+        self.showing_quit_popup = False
         self.paused = False
 
     def run(self, delta_time, events):
         for e in events:
-            if e.type == pygame.KEYDOWN:
-                if e.key == pygame.K_ESCAPE:
+            if self.showing_quit_popup:
+                if e.type == pygame.KEYDOWN:
+                    if e.key == pygame.K_ESCAPE:
+                        return "MENU"
+                    elif e.key == pygame.K_BACKSPACE:
+                        self.showing_quit_popup = False
+                
+                if self.exit_btn.is_clicked(e):
                     return "MENU"
-                elif e.key == pygame.K_p:
-                    self.paused = not self.paused
+                
+                elif self.go_back_btn.is_clicked(e):
+                    self.showing_quit_popup = False
+            else:
+                if e.type == pygame.KEYDOWN:
+                    if e.key == pygame.K_ESCAPE:
+                        self.showing_quit_popup = True
+                    elif e.key == pygame.K_p:
+                        self.paused = not self.paused
 
-            elif e.type == self.METEOR_EVENT and not self.paused:
-                pos = (randint(20, int(self.screen_width)), randint(-85, -60))
-                Meteor(self.meteors_group, self.lasers_group, self.meteor_frames, self.explosion_sound, pos, self.screen_height)
-        
-            elif e.type == self.FRAGMENT_EVENT and not self.paused:
-                pos = (randint(20, int(self.screen_width)), randint(-85, -60))
-                image = choice(self.fragment_images)
-                Fragment(self.fragment_group, image, pos, self.screen_height, self.fragment_pickup)
+                elif e.type == self.METEOR_EVENT and not self.paused and not self.showing_quit_popup:
+                    pos = (randint(20, int(self.screen_width)), randint(-85, -60))
+                    Meteor(self.meteors_group, self.lasers_group, self.meteor_frames, self.explosion_sound, pos, self.screen_height)
+            
+                elif e.type == self.FRAGMENT_EVENT and not self.paused and not self.showing_quit_popup:
+                    pos = (randint(20, int(self.screen_width)), randint(-85, -60))
+                    image = choice(self.fragment_images)
+                    Fragment(self.fragment_group, image, pos, self.screen_height, self.fragment_pickup)
 
-            elif e.type == self.POWERUP_EVENT and not self.paused:
-                pos = (randint(20, int(self.screen_width)), randint(-85, -60))
-                type = choice(self.PowerUp_Choices)
-                image = self.PowerUp_Dict[type]
+                elif e.type == self.POWERUP_EVENT and not self.paused and not self.showing_quit_popup:
+                    pos = (randint(20, int(self.screen_width)), randint(-85, -60))
+                    type = choice(self.PowerUp_Choices)
+                    image = self.PowerUp_Dict[type]
 
-                PowerUp(self.powerups_group, image, pos, type, self.screen_height)
+                    PowerUp(self.powerups_group, image, pos, type, self.screen_height)
 
         self.screen.blit(self.background, [0, 0])
 
         # ? --- Update Groups
-        if not self.paused:
+        if not self.paused and not self.showing_quit_popup:
             self.stars_group.update()
             self.meteors_group.update(delta_time)
             self.fragment_group.update(delta_time)
@@ -81,12 +96,22 @@ class Play:
         self.player_group.draw(self.screen)
 
         if self.paused:
-            draw_text(self.screen, self.title_font, "Juego Pausado", "white", self.screen_width // 2, self.screen_height // 2)
+            draw_text(self.screen, self.title_font, "Game Paused", "white", self.screen_width // 2, self.screen_height // 2)
 
         if self.player.health <= 0:
             return "GAMEOVER"
         elif self.player.fragments >= self.fragment_goal:
             return "WIN"
+        
+        if self.showing_quit_popup: # -> Exit
+            self.screen.blit(self.scrim, [0, 0])
+            pygame.draw.rect(self.screen, "#2A183B", self.popup_rect)
+            pygame.draw.rect(self.screen, "#9D4EFF", self.popup_rect, 2)
+
+            draw_text(self.screen, self.popup_title_font, "SIGNAL LOST", "white", self.screen_width // 2, self.screen_height // 2 - 95)
+            draw_text(self.screen, self.popup_description_font, "Core Fragments will remain uncollected. Confirm exit?", "#D0B8E8", self.screen_width // 2, self.screen_height // 2)
+            self.exit_btn.draw()
+            self.go_back_btn.draw()
 
         return "PLAY"
     
@@ -153,10 +178,23 @@ class Play:
     def setup_ui(self):
         background_img = pygame.image.load(join(self.wd, "assets", "images", "menu", "space.png"))
         self.background = pygame.transform.scale(background_img, (self.screen_width, self.screen_height)).convert()
+        self.button_clicked_sound = pygame.Sound(join(self.wd, "assets", "audio", "button_clicked.mp3"))
+        self.button_clicked_sound.set_volume(0.2)
 
         self.HealthBar = HealthBar(self.heart, (42, 20), 3)
-        self.TimeCounter = TimeCounter(self.screen_width / 2 - 80, 25, 160, 80, self.time_counter_font)
+        self.TimeCounter = TimeCounter(self.screen_width / 2 - 80, 25, 240, 80, self.time_counter_font)
         self.FragmentCounter = ResourceCounter(self.resource_counter_font, "Core Fragments", "white", (60, 180), self.fragments, self.fragment_goal)
+
+        # ? --- Quit PopUp ---
+        self.scrim = pygame.Surface((self.screen_width, self.screen_height), pygame.SRCALPHA)
+        self.scrim.fill((0, 0, 0, 150))
+        self.popup_rect = pygame.FRect(0, 0, self.screen_width - 325, self.screen_height // 3)
+        self.popup_rect.center = (self.screen_width // 2, self.screen_height // 2)
+
+        self.exit_btn = ButtonUI(self.screen, (self.screen_width // 2 - 155, self.screen_height // 2 + 95), "#A62056", "#FF3080",
+                                self.popup_button_font, "Exit", 200, 45, self.button_clicked_sound)
+        self.go_back_btn = ButtonUI(self.screen, (self.screen_width // 2 + 155, self.screen_height // 2 + 95), "#5A3896", "#8A60D0",
+                                self.popup_button_font, "Go Back", 200, 45, self.button_clicked_sound)
 
     def setup_fonts(self):
         vt323_path = join(self.wd, "assets", "fonts", "VT323", "VT323-Regular.ttf")
@@ -164,3 +202,8 @@ class Play:
         self.title_font = pygame.font.Font(vt323_path, 124)
         self.time_counter_font = pygame.font.Font(vt323_path, 84)
         self.resource_counter_font = pygame.font.Font(vt323_path, 32)
+
+        # ? --- Quit PopUp Fonts ---
+        self.popup_title_font = pygame.font.Font(vt323_path, 84)
+        self.popup_description_font = pygame.font.Font(vt323_path, 62)
+        self.popup_button_font = pygame.font.Font(vt323_path, 42)
