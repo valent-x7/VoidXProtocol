c874590a20a9674f8fb8870ae646b685378b1c28
VacaMuu7 <valentinvacacipres@gmail.com>
2025-12-07 12:31:36 -0600

    readme.

--- src/main.py
+++ src/main.py
@@ -1,3 +1,11 @@
+# -----------------------------------------------------------
+# VoidXProtocol
+# A retro space shooter game developed with Pygame.
+# 
+# (C) 2025 Valentin Vaca
+# Released under the MIT License
+# -----------------------------------------------------------
+
 import pygame
 from game import Game
 pygame.init()
